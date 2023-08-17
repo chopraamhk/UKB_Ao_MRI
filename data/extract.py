d84fc9ed272a8e05c658ae8236fea37f18ae46ba
Mehak Chopra <mhk3chopra18@gmail.com>
2023-08-17 19:04:26 +0100

    Update extract.py

--- data/extract.py
+++ data/extract.py
@@ -12,7 +12,7 @@ filtered_data = data[data['20210-2.0'].str.contains('20210_2', na=False)]
 values_in_column1 = filtered_data['eid']
 
 # Create a new DataFrame with the extracted values
-output_data = pd.DataFrame({'Extracted Values': values_in_column1})
+output_data = pd.DataFrame({'eid': values_in_column1})
 
 # Write the output DataFrame to a CSV file
 output_data.to_csv('instance1.csv', index=False)  # Replace 'output_values.csv' with your desired output file name
